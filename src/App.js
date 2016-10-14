4e5c5a461e77797b7362de149dcfe3584e77f2c8
phipsuk <phips.uk@gmail.com>
2016-10-14 15:40:30 +0100

    added logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './grouping-logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          
         </div>
+        <div className="Content">
+        <h2>Welcome to grouPing</h2>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </div>
       </div>
     );
   }
